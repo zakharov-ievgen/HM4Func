0c8c41d586c99e287062402f92938cc03c011102
zakharov-ievgen <zakharov.ievgen.ukr@gmail.com>
2022-05-14 21:03:15 +0300

    Done

---
+++ js/javascript1.js
@@ -0,0 +1,21 @@
+function PrimeOrNot() {
+    for (let i = 0; i < 10; i++) {
+        let value = prompt("Enter your number!");
+        if (value < 100) {
+            continue;
+        } else {
+            return console.log(value);
+        }
+    }
+}
+
+function AgeSelector() {
+    let answer;
+    let age = prompt("How old are you?");
+    if (18 <= age && age <= 100) {
+        answer = "Ura";
+    } else {
+        answer = "false";
+    }
+    console.log(answer);
+}
